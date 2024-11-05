1b71ee7baef0f6fe32d4de5af0b13189eb1037d6
Carter Schmidt <fyrecean@gmail.com>
2024-11-05 02:03:54 -0800

    Add wgsl loading

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,8 +7,13 @@ module.exports = {
     module: {
         rules: [
             {
-                use: "ts-loader",
-                exclude: /node_modules/,
+                test: /\.wgsl$/,         // Apply raw-loader to .wgsl files
+                use: 'raw-loader',
+            },
+            {
+                test: /\.ts$/,           // Apply ts-loader to .ts files
+                use: 'ts-loader',
+                exclude: /node_modules/, // Exclude node_modules from ts-loader
             },
         ],
     },
